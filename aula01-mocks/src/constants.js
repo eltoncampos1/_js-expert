20fb83c881a54c72d40dc91e5e31f6cb6d1fc8c7
elton de campos <eltoncampos36@gmail.com>
2021-06-09 23:31:09 -0300

    chore: create file to to have constants helpers

---
+++ aula01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constans = {
+  error: {
+    FILE_LENGHT_ERROR_MESSAGE: "The content lenght is invalid",
+    FILE_FIELDS_ERROR_MESSAGE: "The provided properties are invalid!",
+  },
+};
+
+module.exports = constans;
