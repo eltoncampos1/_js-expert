44c2c65e2504849c56140a5a26dd9ff073661aff
elton de campos <eltoncampos36@gmail.com>
2021-06-09 23:18:05 -0300

    chore: create function to get File content

--- aula01-mocks/src/file.js
+++ aula01-mocks/src/file.js
@@ -1,10 +1,20 @@
+const { readFile } = require("fs/promises");
+const { join } = require("path");
+
 class File {
   static async cvsToJson(filePath) {
-    return "heloo";
+    const content = await File.getFileContent(filePath);
+    return content;
+  }
+
+  static async getFileContent(filePath) {
+    const fileName = join(__dirname, filePath);
+    return (await readFile(fileName)).toString("utf8");
   }
 }
 
 (async () => {
-  const result = await File.cvsToJson("./../mocks/theeitems-valid.csv");
+  // const result = await File.cvsToJson("../mocks/threeItems-valid.csv");
+  const result = await File.cvsToJson("../mocks/fourItems-invalid.csv");
   console.log("result", result);
 })();
