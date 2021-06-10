128b9c2c6cff2ad5a4c2280f3d761b1fab059874
elton de campos <eltoncampos36@gmail.com>
2021-06-09 23:31:40 -0300

    chore: create function to valid if header are not invalid

--- aula01-mocks/src/file.js
+++ aula01-mocks/src/file.js
@@ -1,9 +1,17 @@
 const { readFile } = require("fs/promises");
 const { join } = require("path");
 
+const { error } = require("./constants");
+
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"],
+};
 class File {
   static async cvsToJson(filePath) {
     const content = await File.getFileContent(filePath);
+    const validation = File.isValid(content);
+    if (!validation.valid) throw new Error(validation.error);
     return content;
   }
 
@@ -11,10 +19,23 @@ class File {
     const fileName = join(__dirname, filePath);
     return (await readFile(fileName)).toString("utf8");
   }
+
+  static isValid(csvString, options = DEFAULT_OPTIONS) {
+    const [header, ...fileWithoutHeaders] = csvString.split("\r\n");
+    const isHeaderValid = header === options.fields.join(",");
+
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+  }
 }
 
 (async () => {
   // const result = await File.cvsToJson("../mocks/threeItems-valid.csv");
-  const result = await File.cvsToJson("../mocks/fourItems-invalid.csv");
+  // const result = await File.cvsToJson("../mocks/fourItems-invalid.csv");
+  const result = await File.cvsToJson("../mocks/invalid-header.csv");
   console.log("result", result);
 })();
